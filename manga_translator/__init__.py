149daec3f4d5592e824d7bb71a6de7407d58eb25
yuhanyang <yuhanyang@futunn.com>
2026-04-17 18:09:13 +0800

    fix: invalid character '\x1b'

--- manga_translator/__init__.py
+++ manga_translator/__init__.py
@@ -1,7 +1,16 @@
 import colorama
+import os
+import sys
 from dotenv import load_dotenv
 
-colorama.init(autoreset=True)
+# Avoid ANSI escape sequences when stdout is non-interactive (e.g. cog openapi_schema),
+# otherwise Cog may fail to parse JSON output with "\x1b" errors.
+_NO_COLOR = (
+    os.getenv("NO_COLOR") is not None
+    or os.getenv("COG_NO_COLOR") is not None
+    or not sys.stdout.isatty()
+)
+colorama.init(autoreset=not _NO_COLOR, strip=_NO_COLOR)
 load_dotenv()
 
 from .manga_translator import *
