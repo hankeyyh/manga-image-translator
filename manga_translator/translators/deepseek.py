33248e5ec0647c85ac8c7945891bdfc427f4bc7d
yuhanyang <yuhanyang@futunn.com>
2026-08-24 22:13:31 +0800

    chore: 调整deepseek超时时间

--- manga_translator/translators/deepseek.py
+++ manga_translator/translators/deepseek.py
@@ -18,7 +18,7 @@ from .tokenizers.token_counters import deepseekTokenCounter
 class DeepseekTranslator(CommonGPTTranslator):
     _INVALID_REPEAT_COUNT = 0  # 现在这个参数没意义了
     _MAX_REQUESTS_PER_MINUTE = 9999  # 无RPM限制
-    _TIMEOUT = 40  # 在重试之前等待服务器响应的时间（秒）
+    _TIMEOUT = 60  # 在重试之前等待服务器响应的时间（秒）
     _RETRY_ATTEMPTS = 3  # 在放弃之前重试错误请求的次数
     _TIMEOUT_RETRY_ATTEMPTS = 3  # 在放弃之前重试超时请求的次数
     _RATELIMIT_RETRY_ATTEMPTS = 3  # 在放弃之前重试速率限制请求的次数
